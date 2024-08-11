ba474c58dc3fe086b3617ff1a198c407defbfb46
shap0011 <shap0011@algonquinlive.com>
2024-08-11 15:29:14 -0400

    edits plugins.js file

--- config/plugins.js
+++ config/plugins.js
@@ -9,4 +9,19 @@ module.exports = ({ env }) => ({
       gitBranch: env('GIT_BRANCH')
     }
   },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_API_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
 });
