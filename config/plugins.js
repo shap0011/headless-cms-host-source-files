592f43d10dd100c4a63710097ac91cea0d310fb7
shap0011 <shap0011@algonquinlive.com>
2024-08-11 15:09:34 -0400

    edits .env and pluguns.js files

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,12 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+
+  'strapi-neon-tech-db-branches': {
+    enabled: true,
+    config: {
+      neonApiKey: env('NEON_API_KEY'),
+      neonProjectName: env('NEON_PROJECT_NAME'),
+      neonRole: env('NEON_ROLE'),
+      gitBranch: env('GIT_BRANCH')
+    }
+  },
+});
